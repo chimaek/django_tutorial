91d97643a43bc7f43e0385a8053fc4e952e777d7
seungju.kim <pipiru100@gmail.com>
2024-01-17 12:17:14 +0900

    http 404 발생

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render
 from .models import Question
 
@@ -6,16 +6,21 @@ from .models import Question
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return render(request, "index.html", {"latest_question_list": latest_question_list})
 
 
 def detail(request, question_id):
-    return HttpResponse(f"You're looking at question {question_id}.")
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("퀘스천이 없어요")
+    return render(request, "detail.html", {"question": question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+    pass
 
 
 def vote(request, question_id):
