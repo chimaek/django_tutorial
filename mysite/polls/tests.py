5432eeb6f8586c15debbde5cfa1ddff81cf23bee
seungju.kim <pipiru100@gmail.com>
2024-01-19 10:58:53 +0900

    add files

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,34 @@
+import datetime
+
 from django.test import TestCase
+from django.utils import timezone
+
+from .models import Question
+
 
 # Create your tests here.
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
+
+    def test_question_count(self):
+        Question.objects.create(question_text="test",pub_date=timezone.now())
+        self.assertEqual(Question.objects.count(),1)
+
+    def test_question_instance(self):
+        Question.objects.create(question_text="test",pub_date=timezone.now())
+        self.assertIsInstance(Question.objects.first(),Question)
+
+    def test_question_update(self):
+        question = Question.objects.create(question_text="test",pub_date=timezone.now())
+        question.question_text = "update"
+        question.save()
+        self.assertEqual(Question.objects.first().question_text,"update")
+
+    def test_delete_question(self):
+        question = Question.objects.create(question_text="test",pub_date=timezone.now())
+        question.delete()
+        self.assertEqual(Question.objects.count(),0)
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,28 +1,29 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 from .models import Question, Choice
 
 
 # Create your views here.
 
-def index(request):
-    latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    output = ", ".join([q.question_text for q in latest_question_list])
-    return render(request, "index.html", {"latest_question_list": latest_question_list})
+class IndexView(generic.ListView):
+    template_name = "index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+
+        return Question.objects.order_by("-pub_date")[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "detail.html"
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, "detail.html", {"question": question})
-
-
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request,"results.html",
-                  {"question":question})
-
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "results.html"
 
 def vote(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
